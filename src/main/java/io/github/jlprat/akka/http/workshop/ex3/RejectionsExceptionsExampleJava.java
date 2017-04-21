389efc744df8057bebc3aa9c233573f7b601f52a
Josep Prat <jlprat@gmail.com>
2017-04-21 17:01:37 +0200

    provide java implementation

--- src/main/java/io/github/jlprat/akka/http/workshop/ex3/RejectionsExceptionsExampleJava.java
+++ src/main/java/io/github/jlprat/akka/http/workshop/ex3/RejectionsExceptionsExampleJava.java
@@ -1,7 +1,7 @@
 package io.github.jlprat.akka.http.workshop.ex3;
 
-import akka.http.javadsl.server.HttpApp;
-import akka.http.javadsl.server.Route;
+import akka.http.javadsl.model.StatusCodes;
+import akka.http.javadsl.server.*;
 
 import java.util.concurrent.ExecutionException;
 
@@ -12,7 +12,36 @@ import java.util.concurrent.ExecutionException;
 public class RejectionsExceptionsExampleJava extends HttpApp {
     @Override
     protected Route route() {
-        return null;
+        final RejectionHandler rejectionHandler = RejectionHandler.newBuilder()
+                .handle(MethodRejection.class, rej ->
+                        complete(StatusCodes.METHOD_NOT_ALLOWED, "Have you tried with more conventional methods?")
+                )
+                .handleNotFound(complete(StatusCodes.NOT_FOUND, "Nothing to see here!"))
+                .build();
+        final ExceptionHandler exceptionHandler = ExceptionHandler.newBuilder()
+                .match(ArithmeticException.class, ex ->
+                        complete(StatusCodes.INTERNAL_SERVER_ERROR, "Do you math?")
+                ).build();
+        return handleRejections(rejectionHandler, () ->
+                route(
+                        get(() ->
+                                path("onlyget", () ->
+                                        complete("got it")
+                                )
+                        ),
+                        handleExceptions(exceptionHandler, () ->
+                                route(
+                                        path("zerodivision", () ->
+                                                complete(Integer.toString(2 / 0))
+                                        ),
+                                        path("crash", () -> {
+                                                    throw new RuntimeException();
+                                                }
+                                        )
+                                )
+                        )
+                )
+        );
     }
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
