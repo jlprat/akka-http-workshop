ce2535898a7129e16a2e9a15c7bfbb607b33022d
Josep Prat <jlprat@gmail.com>
2018-10-29 22:00:32 +0100

    Add example of flawed logging response time

---
+++ src/main/scala/io/github/jlprat/akka/http/workshop/logRequest/LogRequestResponse.scala
@@ -0,0 +1,53 @@
+package io.github.jlprat.akka.http.workshop.logRequest
+
+import akka.event.{Logging, LoggingAdapter}
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpRequest}
+import akka.http.scaladsl.server.RouteResult.{Complete, Rejected}
+import akka.http.scaladsl.server.directives.{LogEntry, LoggingMagnet}
+import akka.http.scaladsl.server.{Directive0, HttpApp, Route, RouteResult}
+import akka.stream.scaladsl.Source
+import akka.util.ByteString
+import spray.json.DefaultJsonProtocol
+
+
+class LogRequestResponse extends HttpApp with DefaultJsonProtocol {
+
+  private val lorem = List("lorem", " ", "ipsum", " ", "dolor", " ", "sit", " ", "amet", "\n").map(ByteString.fromString)
+
+  private val infiniteLorems = Source.cycle(() => lorem.iterator)
+
+  private def logTimeToSendStartResponse(loggingAdapter: LoggingAdapter, requestTimestamp: Long)
+                                        (req: HttpRequest)(res: RouteResult): Unit = {
+    val entry = res match {
+      case Complete(resp) =>
+        val responseTimestamp: Long = System.nanoTime
+        val elapsedTime: Long = (responseTimestamp - requestTimestamp) / 1000
+        LogEntry(s"""Logged Request:${req.method}:${req.uri}:${resp.status}:${elapsedTime}µs""", Logging.InfoLevel)
+      case Rejected(reason) =>
+        LogEntry(s"Rejected Reason: ${reason.mkString(",")}", Logging.InfoLevel)
+    }
+    entry.logTo(loggingAdapter)
+  }
+
+  private def printResponseTime(log: LoggingAdapter): HttpRequest => RouteResult => Unit = {
+    val requestTimestamp = System.nanoTime
+    logTimeToSendStartResponse(log, requestTimestamp)(_)
+  }
+
+  private val logResponseTime: Directive0 = logRequestResult(LoggingMagnet(printResponseTime))
+
+  override protected def routes: Route = logResponseTime {
+    concat(
+      path("finite") {
+        complete("Done!")
+      },
+      path("infinite") {
+        complete(HttpEntity.Chunked.fromData(ContentTypes.`text/plain(UTF-8)`, infiniteLorems))
+      }
+    )
+  }
+}
+
+object LogRequestResponse extends App {
+  new LogRequestResponse().startServer("localhost", 9000)
+}
